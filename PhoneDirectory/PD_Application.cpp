0b608197a3cf2601af99dff7936d09eec21d192e
smaxa20 <smaxa20@LAB-SCI-304-16>
2017-09-17 15:48:46 -0700

    do_remove_entry

--- PhoneDirectory/PD_Application.cpp
+++ PhoneDirectory/PD_Application.cpp
@@ -87,11 +87,10 @@ void do_lookup_entry(Phone_Directory& the_directory)
 
 void do_remove_entry(Phone_Directory& the_directory) // Exercise 1.8: please complete the function do_remove_entry - Ed/Kent
 {
-	string name;
+	string name;							// Scott Maxa
 	cout << "Enter name: ";
 	getline(cin, name);
-	// Complete the rest of this function 
-	
+	the_directory.remove_entry(name);
 }
 
 void do_save(Phone_Directory& the_directory)
